0a4bb7899d52b71a954923ea85556c8dd768b83f
zhujia <izhujia@vip.qq.com>
2018-05-20 20:26:45 +0800

    打印三角形

--- src/main/java/algorithm/PrintTriangle.java
+++ src/main/java/algorithm/PrintTriangle.java
@@ -16,16 +16,16 @@ public class PrintTriangle {
         System.out.println("请输入数字n");
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {//控制行数
-            for (int j = 0; j <= i; j++) {//控制列数
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 int value = j+1;
-                for (int k = 0; k < i-j; k++) {//控制输出数字
+                for (int k = 0; k < i-j; k++) {
                     value = value + (n-k);
                 }
                 System.out.print(value+" ");
 
             }
-            System.out.println();//打印换行
+            System.out.println();
         }
     }
 }
